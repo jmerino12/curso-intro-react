61dc15332f042a4231d1e405f69dbd2d0f1e9655
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-03-18 09:35:13 -0500

    Contando y buscando TODOs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { TodoCounter } from "./TodoCounter";
 import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
-// import './App.css';
 
-const todos = [
+const defaultTodos = [
   { text: "Cortar cebolla", completed: true },
   { text: "Tomar el cursso de intro a React", completed: false },
   { text: "Llorar con la llorona", completed: false },
@@ -14,13 +13,30 @@ const todos = [
 ];
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [todos, setTodos] = useState(defaultTodos);
+  const completedTodos = todos.filter((todo) => todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if (!search.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter((todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = search.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter completedTodos={completedTodos} totalTodos={totalTodos} />
+      <TodoSearch search={search} setSearch={setSearch} />
 
       <TodoList>
-        {todos.map((todo) => (
+        {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
             text={todo.text}
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import "./TodoSearch.css";
 
-function TodoSearch() {
-  const [search, setSearch] = useState("");
+function TodoSearch({ search, setSearch }) {
   const onSearchValueChange = ({ target }) => {
     setSearch(target.value);
+    console.log(target.value);
   };
   return (
     <input
